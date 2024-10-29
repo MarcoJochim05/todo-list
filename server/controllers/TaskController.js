e22543f7332ab38285db3932947050462075027e
efe okyar <f.efe.okyar@gmail.com>
2024-10-29 17:01:38 +0200

    first commit

---
+++ server/controllers/TaskController.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-unused-vars */
+import { insertTask, selectAllTasks } from "../models/Task.js";
+import { emptyOrRows } from "../helper/utils.js";
+
+import { deleteTask } from "../models/Task.js";
+
+const getTasks = async (req, res, next) => {
+  try {
+    const result = await selectAllTasks();
+    return res.status(200).json(emptyOrRows(result));
+  } catch (error) {
+    return next(error);
+  }
+};
+
+const postTask = async (req, res, next) => {
+  try {
+    if (!req.body.description || req.body.description.length === 0) {
+      const error = new Error("Invalid Description for task");
+      error.statusCode = 400;
+      return next(error);
+    }
+    const result = await insertTask(req.body.description);
+    return res.status(200).json({ id: result.rows[0].id });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+const taskDelete = async (req, res, next) => {
+  console.log("Task Delete!");
+  try {
+    const id = parseInt(req.params.id);
+
+    const result = await deleteTask(id);
+
+    return res.status(200).json({ id: id });
+  } catch (error) {
+    console.log("Error", error);
+    return next(error);
+  }
+};
+
+export { getTasks, postTask, taskDelete };
---
+++ server/helper/test.js
@@ -0,0 +1,35 @@
+import fs from "fs";
+import path from "path";
+
+import { pool } from "./db.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { hash } from "bcrypt";
+import pkg from "jsonwebtoken";
+const { sign } = pkg;
+
+// const __dirname = import.meta.dirname;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const initializeTestDb = () => {
+  const sql = fs.readFileSync(
+    path.resolve(__dirname, "../database.sql"),
+    "utf8"
+  );
+  pool.query(sql);
+};
+
+const insertTestUser = (email, password) => {
+  hash(password, 10, (error, hashedPassword) => {
+    pool.query("insert into account (email,password) values ($1,$2)", [
+      email,
+      hashedPassword,
+    ]);
+  });
+};
+
+const getToken = (email) => {
+  return sign({ user: email }, process.env.JWT_SECRET_KEY);
+};
+export { initializeTestDb, insertTestUser, getToken };
